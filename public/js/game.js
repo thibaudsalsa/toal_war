8517615bdf4cf19053cd625913854773b98cec11
Thibaud salsa <thibaud.salsa@upscribers.org>
2018-07-11 14:50:48 +0000

    [ADD] buy tank soldier plane, moving to left ennemies

--- public/js/game.js
+++ public/js/game.js
@@ -1,5 +1,61 @@
-var iso = new Isomer(document.getElementById("art"));
+var soldat = [-1];
+var char = [-1];
+var avion = [-1];
+var my_money = 200;
+//              creation de la map
+function make_game()
+{
+    /*global Isomer*/
+    var iso = new Isomer(document.getElementById("art"));
+    var color = Isomer.Color;
+    var Point = Isomer.Point;
+    var Path = Isomer.Path;
+    var Shape = Isomer.Shape
 
-var green = "57, 255, 20"
+    //               couleur
+    var Color = new Object();
+    Color.green = new color(76, 187, 23);
+    Color.blue = new color(0, 0, 200);
+    Color.red = new color(200, 0, 0);
+    Color.orange = new color(253, 106, 2);
+    
 
-iso.add(Isomer.Shape.Prism(Isomer.Point.ORIGIN, 5, 1, 0.5, green));
+    //              init objet
+    // objet position
+    var pos = new Object();
+    pos.x = 0;
+    pos.y = 0;
+    pos.z = 0;
+    // objet taille
+    var size = new Object();
+    size.x = 0;
+    size.y = 0;
+    size.z = 0;
+    //objet item
+    var form = new Object();
+    form.iso = iso;
+    form.Point = Point;
+    form.color = null;
+    form.pos = pos;
+    form.size = size;
+    form.path = Path;
+    form.shape = Shape;
+    form = set_pos(form, 0, 0, 0);
+    form = set_size(form, 8, 8, 0.2);
+    
+    
+    //create map
+    make_battleground(form, Color);
+    create_castel(form, Color);
+    for (let i = 0; soldat[i] != -1; i++)
+        soldat[i] = soldier(form, Color, soldat[i]);
+    for (let i = 0; avion[i] != -1; i++)
+        avion[i] = plane(form, Color, avion[i]);
+    for (let i = 0; char[i] != -1; i++)
+        char[i] = tank(form, Color, char[i]);
+    my_money += 0.01;
+    document.getElementById("argent").innerHTML = "Argent: " + parseInt(my_money, 10);
+    requestAnimationFrame(make_game)
+}
+
+make_game();
---
+++ public/js/game_lib.js
@@ -0,0 +1,134 @@
+//              fonction de creation d'objet
+//move size
+function set_size(form, x, y, z)
+{
+    form.size.x = x;
+    form.size.y = y;
+    form.size.z = z;
+    return (form);
+}
+//move pos
+function set_pos(form, x, y, z)
+{
+    form.pos.x = x;
+    form.pos.y = y;
+    form.pos.z = z;
+    return (form);
+}
+//creation de la map
+function make_battleground(form, color)
+{
+    var battleground = form.shape.Prism(form.Point(form.pos.x, form.pos.y, form.pos.z), form.size.x, form.size.y, form.size.z);
+    form.iso.add(battleground, color.green);
+}
+
+function soldier(form, color, soldx)
+{
+    if (soldx >= 6)
+        soldx = 6;
+    form = set_pos(form, 1, soldx, 0);
+    form = set_size(form, 0.1, 0.1, 0.4);
+    var soldier = form.shape.Prism(form.Point(form.pos.x, form.pos.y, form.pos.z), form.size.x, form.size.y, form.size.z);
+    form.iso.add(soldier, color.blue);
+    soldx += 0.007;
+    return soldx;
+}
+
+function plane(form, color, planx)
+{
+    if (planx >= 5)
+        planx = 5;
+    form = set_pos(form, 1, planx, 2);
+    form = set_size(form, 0.1, 0.4, 0.1);
+    var soldier = form.shape.Prism(form.Point(form.pos.x, form.pos.y, form.pos.z), form.size.x, form.size.y, form.size.z);
+    form.iso.add(soldier, color.blue);
+    planx += 0.01;
+    return planx
+}
+
+function tank(form, color, tankx)
+{
+    if (tankx >= 6)
+        tankx = 6;
+    form = set_pos(form, 1, tankx, 0);
+    form = set_size(form, 0.2, 0.2, 0.1);
+    var soldier = form.shape.Prism(form.Point(form.pos.x, form.pos.y, form.pos.z), form.size.x, form.size.y, form.size.z);
+    form.iso.add(soldier, color.blue);
+    tankx += 0.003;
+    return tankx;
+}
+
+function create_castel(form, color)
+{
+    var briq;
+    var tmp_c = color.blue;
+    form = set_pos(form, 1, 1, 0);
+    for (let j = 0; j < 3; j += 1)
+    {
+        if (j === 1)
+        {
+            tmp_c = color.red;
+            form = set_pos(form, 7, 1, 0);
+        }
+        if (j === 2)
+        {
+            tmp_c = color.orange;
+            form = set_pos(form, 1, 7, 0);
+        }
+        form = set_size(form, 0.25, 0.25, 1.8);
+        //big building
+        briq = form.shape.Prism(form.Point(form.pos.x, form.pos.y, form.pos.z), form.size.x, form.size.y, form.size.z);
+        form.iso.add(briq, tmp_c);
+        form.pos.x += 0.7;
+        form = set_size(form, 0.25, 0.25, 0.9);
+        //medium building
+        briq = form.shape.Prism(form.Point(form.pos.x, form.pos.y, form.pos.z), form.size.x, form.size.y, form.size.z);
+        form.iso.add(briq, tmp_c);
+        form.pos.x -= 0.5;
+        form.pos.y -= 0.7;
+        form = set_size(form, 0.25, 0.25, 0.5);
+        //small building
+        briq = form.shape.Prism(form.Point(form.pos.x, form.pos.y, form.pos.z), form.size.x, form.size.y, form.size.z);
+        form.iso.add(briq, tmp_c);
+        //litte building
+        form.pos.x -= 0.7;
+        form.pos.y += 0.7;
+        form = set_size(form, 0.25, 0.25, 0.2);
+        briq = form.shape.Prism(form.Point(form.pos.x, form.pos.y, form.pos.z), form.size.x, form.size.y, form.size.z);
+        form.iso.add(briq, tmp_c);
+    }
+}
+
+/*global my_money*/
+/*global soldat*/
+function add_soldier()
+{
+    if (my_money < 50)
+        return;
+    for (var i = 0; soldat[i] != -1; i++);
+    soldat[i] = 1.3;
+    soldat.push(-1);
+    my_money -= 50;
+}
+
+/*global char*/
+function add_tank()
+{
+    if (my_money < 100)
+        return;
+    for (var i = 0; char[i] != -1; i++);
+    char[i] = 1.3;
+    char.push(-1);
+    my_money -= 100;
+}
+
+/*global avion*/
+function add_plane()
+{
+    if (my_money < 150)
+        return;
+    for (var i = 0; avion[i] != -1; i++);
+    avion[i] = 1.3;
+    avion.push(-1);
+    my_money -= 150;
+}
