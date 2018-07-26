b636fed1cbaafc3a73a9d3ea16ca827dca90edec
Thibaud salsa <thibaud.salsa@upscribers.org>
2018-07-26 22:53:49 +0000

    [FIXE]

--- ws.js
+++ ws.js
@@ -1,7 +1,7 @@
 var vm = require("vm");
-//var fs = require("fs");
+var fs = require("fs");
 var WebSocketServer = require('ws').Server, wss = new WebSocketServer({port: 40510});
-//vm.runInThisContext(fs.readFileSync(__dirname + "/game_server.js"));
+vm.runInThisContext(fs.readFileSync(__dirname + "/game_server.js"));
 
 /*global connect do_msg game:true respond init_game*/
 var start = false;
