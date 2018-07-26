731be548dab39ed28c044b11f7b03e9059c8f8c9
Thibaud salsa <thibaud.salsa@upscribers.org>
2018-07-26 13:28:48 +0000

    prod

--- game_server.js
+++ game_server.js
@@ -5,7 +5,18 @@ vm.runInThisContext(fs.readFileSync(__dirname + "/fight.js"));
 
 /*global init_game*/
 
+//creation du jeux
 var game = init_game();
+//creation du serveur avec les webSockets
+var WebSocketServer = require('ws').Server, wss = new WebSocketServer({port: 40510});
+
+wss.on('connection', function (ws) {
+  ws.on('message', function (message) {
+    console.log('received: %s', message);
+      ws.send("you are connected");
+      console.log("client disconected");
+  });
+});
 
 function connect(team, game)
 {
