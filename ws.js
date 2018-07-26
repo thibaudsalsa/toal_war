dd83fdf391b1bfca6cf0e434c5abb628dd04f4c0
Thibaud salsa <thibaud.salsa@upscribers.org>
2018-07-26 18:39:58 +0000

    [TRY] websocket

--- ws.js
+++ ws.js
@@ -3,10 +3,7 @@ var WebSocketServer = require('ws').Server, wss = new WebSocketServer({port: 405
 wss.on('connection', function (ws) {
   ws.on('message', function (message) {
     console.log('received: %s', message);
-    try
-    {
       ws.send("you are connected");
-    }
-    catch (er) {console.log("client disconected");}
+    console.log("client disconected");
   });
 });
