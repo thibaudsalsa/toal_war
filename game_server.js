c72422eb9197e107ab1f78153f4324c1e73551b0
Thibaud salsa <thibaud.salsa@upscribers.org>
2018-07-26 18:44:37 +0000

    [ADD] start

--- game_server.js
+++ game_server.js
@@ -13,8 +13,7 @@ var WebSocketServer = require('ws').Server, wss = new WebSocketServer({port: 405
 wss.on('connection', function (ws) {
   ws.on('message', function (message) {
     console.log('received: %s', message);
-      ws.send("you are connected");
-      console.log("client disconected");
+      ws.send("start");
   });
 });
 
