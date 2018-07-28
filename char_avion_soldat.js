7b596bf900dcdb6bc11a35be83051c3b38672752
Thibaud salsa <thibaud.salsa@upscribers.org>
2018-07-28 19:23:06 +0000

    [FIXE] SA MERE

--- char_avion_soldat.js
+++ char_avion_soldat.js
@@ -81,7 +81,7 @@ function create_char(r, g, b, pos_unit)
 {
 	var char = new Object();
 	char.pv = 3;
-	char.dmg = 1 / 100;
+	char.dmg = 1 / 500;
 	char.speed = 1 / 2;
 	char.color = [r, g, b];
 	char.x = pos_unit[0];
@@ -98,7 +98,7 @@ function create_soldat(r, g, b, pos_unit)
 {
 	var soldat = new Object();
 	soldat.pv = 1;
-	soldat.dmg = 3 / 100;
+	soldat.dmg = 3 / 500;
 	soldat.speed = 1 / 2;
 	soldat.color = [r, g, b];
 	soldat.x = pos_unit[0];
@@ -115,7 +115,7 @@ function create_avion(r, g, b, pos_unit)
 {
 	var avion = new Object();
 	avion.pv = 1;
-	avion.dmg = 1 / 100;
+	avion.dmg = 1 / 500;
 	avion.speed = 3 / 2;
 	avion.color = [r, g, b];
 	avion.x = pos_unit[0];
