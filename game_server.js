6667558acea1be82c7b9cb586d921951caa9d5e2
Thibaud salsa <thibaud.salsa@upscribers.org>
2018-07-29 12:45:54 +0000

    [FIXE] money speed

--- game_server.js
+++ game_server.js
@@ -133,11 +133,11 @@ function respond(team, start, ws, wss)
     msg.money = 0;
     msg.info = game.info;
     if (game.team1.city > 0)
-        game.team1.money += 0.014;
+        game.team1.money += 0.024;
     if (game.team2.city > 0)
-        game.team2.money += 0.014;
+        game.team2.money += 0.024;
     if (game.team3.city > 0)
-        game.team3.money += 0.014;
+        game.team3.money += 0.024;
     fill_msg(msg, team, game);
     check_win(game, ws, msg, start);
     msg_json = JSON.stringify(msg);
