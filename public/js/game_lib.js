ea5eb747e106145ed2bd02af38d7d02126753ac2
Thibaud salsa <thibaud.salsa@upscribers.org>
2018-07-30 14:19:54 +0000

    [FIXE] designe

--- public/js/game_lib.js
+++ public/js/game_lib.js
@@ -102,7 +102,7 @@ function lobby()
     var iso = new Isomer(document.getElementById("canvas01"));
     var Shape = Isomer.Shape;
     var Point = Isomer.Point;
-    var Color = Isomer.color;
+    var Color = Isomer.Color;
     var red = new Color(255, 0, 0);
     iso.add(Shape.Prism(new Point(0, 0, 0), 1, 1, 5, red));
 }
