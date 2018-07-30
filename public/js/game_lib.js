fb4878a4df2db1ff4eaf4bb31a0a691b674b808b
Thibaud salsa <thibaud.salsa@upscribers.org>
2018-07-30 14:56:11 +0000

    [FIXE] designe

--- public/js/game_lib.js
+++ public/js/game_lib.js
@@ -104,17 +104,17 @@ function lobby()
     var Point = Isomer.Point;
     var Color = Isomer.Color;
     var red = new Color(255, 0, 0);
-    iso.add(Shape.Prism(new Point(0, 0, 0), 1, 1, 5), red);
-    iso.add(Shape.Prism(new Point(0, 1.5, -1.5), 1, 1, 4), red);
-    iso.add(Shape.Prism(new Point(1.5, 0, -1.5), 1, 1, 3), red);
-    iso.add(Shape.Prism(new Point(-1, -1, 0), 1, 1, 1), red);
+    iso.add(Shape.Prism(new Point(0, 0, 0), 0.75, 0.75, 5), red);
+    iso.add(Shape.Prism(new Point(0, 1.5, -1.5), 0.75, 0.75, 4), red);
+    iso.add(Shape.Prism(new Point(1.5, 0, -1.5), 0.75, 0.75, 3), red);
+    iso.add(Shape.Prism(new Point(-1, -1, 0), 0.75, 0.75, 1), red);
     iso = new Isomer(document.getElementById("canvas02"));
     Shape = Isomer.Shape;
     Point = Isomer.Point;
     Color = Isomer.Color;
     red = new Color(253, 106, 2);
-    iso.add(Shape.Prism(new Point(0, 0, 0), 1, 1, 5), red);
-    iso.add(Shape.Prism(new Point(0, 1.5, -1.5), 1, 1, 4), red);
-    iso.add(Shape.Prism(new Point(1.5, 0, -1.5), 1, 1, 3), red);
-    iso.add(Shape.Prism(new Point(-1, -1, 0), 1, 1, 1), red);
+    iso.add(Shape.Prism(new Point(0, 0, 0), 0.75, 0.75, 5), red);
+    iso.add(Shape.Prism(new Point(0, 1.5, -1.5), 0.75, 0.75, 4), red);
+    iso.add(Shape.Prism(new Point(1.5, 0, -1.5), 0.75, 0.75, 3), red);
+    iso.add(Shape.Prism(new Point(-1, -1, 0), 0.75, 0.75, 1), red);
 }
